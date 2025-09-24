a9cc8b6bd84f6e87a727cf6b0f760a597b203a4f
Dolfirobots <_viktor_@wildenhainmusik.de>
2025-09-24 20:56:58 +0200

    Implemented the CustomConfigAPI class

---
+++ paper/src/main/java/net/dolfirobots/dolfi/CustomConfigAPI.java
@@ -0,0 +1,72 @@
+package net.dolfirobots.dolfi;
+
+import net.dolfirobots.Main;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+import static net.dolfirobots.dolfi.LogsManager.newError;
+
+public class CustomConfigAPI {
+
+    private final String service = "CustomConfigAPI";
+
+    private File file;
+
+    public CustomConfigAPI(File file) {
+        if (file != null) {
+            this.file = file;
+        } else {
+            newError("CustomConfigAPI","File is null!");
+        }
+    }
+
+    public File getFile() {
+        return file;
+    }
+    public void setFile(File file) {
+        if (file != null) {
+            this.file = file;
+        } else {
+            newError("CustomConfigAPI","File is null!");
+        }
+    }
+
+
+    public YamlConfiguration getConfig() {
+        if (getFile() != null) {
+            if (getFile().exists()) {
+                return YamlConfiguration.loadConfiguration(getFile());
+            }
+        }
+        newError("CustomConfigAPI","File is null or not exists!");
+        return null;
+    }
+    public void setConfig(YamlConfiguration config) {
+        if (getFile() != null) {
+            if (getFile().exists()) {
+                try {
+                    config.save(getFile());
+                } catch (IOException e) {
+                    newError("CustomConfigAPI","§cError by saving the file: §e" + e);
+                    throw new RuntimeException(e);
+                }
+            } else {
+                newError("CustomConfigAPI","File not exists!");
+            }
+        } else {
+            newError("CustomConfigAPI","File is null!");
+        }
+    }
+
+    public void registerConfig(String path) {
+        if (getFile() != null) {
+            if (!getFile().exists()) {
+                Main.getInstance().saveResource(path, false);
+            }
+        } else {
+            newError("CustomConfigAPI","File is null!");
+        }
+    }
+}
